681a382dd19d5706386ba0e56708c9f0215de0d0
Harshavardhan <mamidipaka2003@gmail.com>
2024-05-11 23:06:06 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,8 +1,8 @@
-export const host = "https://boingbox.onrender.com";
-export const loginRoute = `${host}/api/auth/login`;
-export const registerRoute = `${host}/api/auth/register`;
-export const logoutRoute = `${host}/api/auth/logout`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const recieveMessageRoute = `${host}/api/messages/getmsg`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
+export const host = "https://boingbox-1.onrender.com";
+export const loginRoute = "https://boingbox-1.onrender.com/api/auth/login";
+export const registerRoute = "https://boingbox-1.onrender.com/api/auth/register";
+export const logoutRoute = "https://boingbox-1.onrender.com/api/auth/logout";
+export const allUsersRoute = "https://boingbox-1.onrender.com/api/auth/allusers";
+export const sendMessageRoute = "https://boingbox-1.onrender.com/api/messages/addmsg";
+export const receiveMessageRoute = "https://boingbox-1.onrender.com/api/messages/getmsg";
+export const setAvatarRoute = "https://boingbox-1.onrender.com/api/auth/setavatar";
