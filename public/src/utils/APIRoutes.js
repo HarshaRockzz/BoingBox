9f52345e85d1348b3a8b1295ed1418b3c30039ae
Harshavardhan <mamidipaka2003@gmail.com>
2024-05-12 08:33:52 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -4,5 +4,5 @@ export const registerRoute = "https://boingbox-1.onrender.com/api/auth/register"
 export const logoutRoute = "https://boingbox-1.onrender.com/api/auth/logout";
 export const allUsersRoute = "https://boingbox-1.onrender.com/api/auth/allusers";
 export const sendMessageRoute = "https://boingbox-1.onrender.com/api/messages/addmsg";
-export const receiveMessageRoute = "https://boingbox-1.onrender.com/api/messages/getmsg";
+export const recieveMessageRoute = "https://boingbox-1.onrender.com/api/messages/getmsg";
 export const setAvatarRoute = "https://boingbox-1.onrender.com/api/auth/setavatar";
